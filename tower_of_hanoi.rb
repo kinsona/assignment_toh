8328c98ba26bdd5878ad9b3b22d670b0c09b55b5
Adam <adam.kinson@gmail.com>
2015-05-19 08:27:55 -0400

    write basic pseudo code

---
+++ tower_of_hanoi.rb
@@ -0,0 +1,24 @@
+class TowerOfHanoi
+
+#greet user and give instructions
+#set turn count = 0
+#show board
+#prompt for next move
+  #if 'quit', quit game
+  #if invalid move, prompt again
+  #if valid, send move
+
+#when move received
+  #add 1 to turn count
+  #move top disc accordingly
+
+#when move completed
+  #test for victory conditions
+  #prompt for next move...
+
+#when victory conditions met
+  #send congratulations message
+  #quit
+
+
+end
